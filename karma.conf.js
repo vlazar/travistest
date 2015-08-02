7ecf4fdcac93852de07f7f83e61dfad3281a86ac
Vladislav Zarakovsky <vlad.zar@gmail.com>
2015-08-02 18:02:03 +0300

    real fix

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,13 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ? ['ChromeTravisCI'] : ['Chrome'],
+    customLaunchers: {
+      ChromeTravisCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
